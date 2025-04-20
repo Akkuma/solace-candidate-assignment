9c4a460d8b99ae5117299bc0593263e120470bb3
Gregory Waxman <akkuma@users.noreply.github.com>
2025-04-20 17:54:16 -0400

    Changed db schema for phone number & specialties

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,5 @@
 import { sql } from "drizzle-orm";
-import {
-  pgTable,
-  integer,
-  text,
-  jsonb,
-  serial,
-  timestamp,
-  bigint,
-} from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 const advocates = pgTable("advocates", {
   id: serial("id").primaryKey(),
@@ -15,9 +7,11 @@ const advocates = pgTable("advocates", {
   lastName: text("last_name").notNull(),
   city: text("city").notNull(),
   degree: text("degree").notNull(),
-  specialties: jsonb("payload").default([]).notNull(),
+  specialties: text("specialties").array().default([]).notNull(),
   yearsOfExperience: integer("years_of_experience").notNull(),
-  phoneNumber: bigint("phone_number", { mode: "number" }).notNull(),
+  // Based on https://www.mayerdan.com/programming/2017/06/26/db_phone_types &
+	// https://github.com/google/libphonenumber/blob/d89d07b8dabd6b6694d55b8f17319e5caa23ef18/FALSEHOODS.md
+	phoneNumber: varchar("phone_number", { length: 18 }).notNull(),
   createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
 });
 
